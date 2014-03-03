69177344532df8c2bcad1ea598803a7453dd5275
cmndrfatty <hentaiowl@yahoo.com>
2014-03-02 18:22:07 -0800

    Create DateTest.java

---
+++ DateTest.java
@@ -0,0 +1,14 @@
+
+public class DateTest {
+	
+	public static void main( String args[] )
+	{
+	Date date1 = new Date( 03, 02, 2014 );
+
+	System.out.print( "Today's date is: " );
+	date1.displayDate();
+
+	System.out.println(); 
+	} 
+
+}
